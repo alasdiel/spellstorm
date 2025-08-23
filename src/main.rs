77fcc7ef75fbef0d5c7a22cd68e0f43503ae125b
alasdiel <alex.albinda9@gmail.com>
2025-08-23 12:11:53 +0800

    fix: formatting fix

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,26 @@ fn main() {
     app.add_systems(Startup, circle_mesh);
     app.run();
 }
+/*
 
-// GAME SYSTEMS
-// TODO: make modules
-fn camera_system(
-    mut commands: Commands,
-){
-    commands.spawn(Camera2d);    
-    }
+GAME SYSTEMS
+TODO: make modules for each isolated system
+camera
+player
+enemy
+
+*/
+fn camera_system(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
 
 fn circle_mesh(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-){
+) {
     let circle = meshes.add(Circle::new(50.0));
     let color = Color::hsl(0., 0.95, 0.7);
 
-    commands.spawn((
-        Mesh2d(circle),
-        MeshMaterial2d(materials.add(color))
-    ));
+    commands.spawn((Mesh2d(circle), MeshMaterial2d(materials.add(color))));
 }
